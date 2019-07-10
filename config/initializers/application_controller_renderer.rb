c020497f27fe19f11165b454fa7b0865a8b8bd25
岡本憲聖 <okamotokensei@okamotokenshounoMacBook-puro.local>
2019-07-10 16:20:39 +0900

    [Finish]5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '0b020287b5bd46cd4dc86ff79f2e3cf1ce8675f3dc92d39a3b2371734724922291e6413df985b145bd9f43d953c46a28fc58604d1a00d4ff8bf9f33d40c63698'
